de5c0743cb527f3e9d6b79865611e8f817df6b79
Andrew Shapton <alshapton@gmail.com>
2020-02-26 16:44:17 +0000

    fix: uodate

--- tools/replace.zsh
+++ tools/replace.zsh
@@ -8,14 +8,14 @@
 PID=$$
 
 # Generate utterances from the model
-python3 utter.py > utterances.$PID
+python3 utter.py > utterances.tmp
 
 # Relocate READMe.md file to local directory for processing
 cp ../README.md $PID
 
-sed -e '/##DONOTREMOVE##/ {' -e 'r utterances.$PID' -e 'd' -e '}' $PID
 # Replace the placeholder in the README.md file with the formatted content of the utterances
+sed -e '/##DONOTREMOVE##/ {' -e 'r utterances.tmp' -e 'd' -e '}' $PID > ../README.md
 
 # Remove the interim working file
-#rm $PID
-#rm utterances.$PID
+rm $PID
+rm utterances.tmp
